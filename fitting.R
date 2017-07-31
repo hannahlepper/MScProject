e9377d9adbb12157137b3544c25735fbc7145607
Hannah Lepper <hannah.c.lepper@gmail.com>
2017-07-31 11:27:30 +0100

    Tweaks and move section to baseline run fi;e

--- fitting.R
+++ fitting.R
@@ -5,6 +5,8 @@ library(deSolve)
 library(rootSolve)
 library(rlist)
 library(plyr)
+library(readr)
+setwd("C://Users/hanna/Documents/GitHub/MSc project/")
 source("PSmodel.R")
 source("Parameter_combinations.R")
 source("matchclosestMALDIquant.R")
@@ -139,8 +141,8 @@ selectmu <- function(initfittingpars, other_pars){
 }
 
 selectpars <- function(pars) {
-  b <- selectbeta(8:22, c(pars, Mu = 0.06))
-  Mu <- selectmu(seq(0.04,0.07,length.out = 10), c(pars, b = unname(b[1])))
+  b <- selectbeta(3:16, c(pars, Mu = 0.06))
+  Mu <- selectmu(seq(0.03,0.08,length.out = 10), c(pars, b = unname(b[1])))
   df<- as.data.frame(cbind(beta = b[1], Mu = Mu[1], Inc = Mu[2]))
   rownames(df) <-NULL
   return(df)
@@ -151,32 +153,14 @@ selectpars <- function(pars) {
 start <- Sys.time()
 lowrlowk <- selectpars(other_pars_lessblessMu)
 timetaken <- Sys.time() - start
+#25 seconds
 
+start <- Sys.time()
 fittedparams <- adply(tofit, 1, selectpars) #runs across all 12
+timetaken <- Sys.time() - start
+#4.3 minutes
+
+write.csv(fittedparams, "C://Users/hanna/Dropbox/Academic/LSHTM/Project/fittedparms.csv")
 
 
-
-pars <- c(other_pars_lessblessMu, 
-          Mu = as.numeric(lowrlowk["Mu"]), 
-          b = as.numeric(lowrlowk["beta"]))
-
-other_pars_lessblessMu <- c(p=0.01, a=0.11, vf=0.67, vs=0.0005, 
-                               sg=0.45, x=0.65, nc=0.2, theta=0.015,  
-                               Mui=0.3, Mun=0.21, CDR=0.7, 
-                               CDR_survey=CDR_int(CDR = 0.7, cov = 0, sens = 0),
-                               tau=0.91, k = .4, r=0.1,c=0.22, Ic = 0.002, 
-                               survey_interval=5)
-
-#Try to get as close to equilibrium as possible - may or may not use
-
-fastrun <- function(y, pars) {
-  initrun<-runsteady(y=y,times=c(0,Inf), func=PSmodel, parms=pars)
-  yinit<-initrun$y
-  sol_base<-ode(y=yinit,times=seq(0,500, by=0.02),func=PSmodel,parms=pars)
-  sol_base_df <- as.data.frame(sol_base)
-  print(equilibrium_test(sol_base_df))
-  return(sol_base_df)
-}
-
-test <- fastrun(yinit, pars)
 
