8a75ae4046b16789ca09cd053066a962305f3ab1
Hannah Lepper <hannah.c.lepper@gmail.com>
2017-08-01 16:42:08 +0100

    More graphs

--- first graph.R
+++ first graph.R
@@ -14,3 +14,18 @@ lines(efficientsurveyhighcomm$time, efficientsurveyhighcomm$Inc, col = "blue")
 abline(v=survey_times(1)[1])
 abline(v=survey_times(1)[4])
 abline(v=survey_times(1)[7])
+
+plot(efficientsurveylowcomm$time, efficientsurveylowcomm$cases_removed, type = "l", 
+     xlim=c(499,510), col = "red")
+lines(efficientsurveyhighcomm$time, efficientsurveyhighcomm$cases_removed, col = "blue")
+abline(v=survey_times(1)[1])
+abline(v=survey_times(1)[4])
+abline(v=survey_times(1)[7])
+
+plot(efficientsurveylowcomm$time, efficientsurveylowcomm$Prev, type = "l", 
+     xlim=c(499,510), col = "red")
+lines(efficientsurveyhighcomm$time, efficientsurveyhighcomm$Prev, col = "blue")
+abline(v=survey_times(1)[1])
+abline(v=survey_times(1)[4])
+abline(v=survey_times(1)[7])
+
