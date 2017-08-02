b688e690baedd3fb83ff33e19504088dc557c4ec
Hannah Lepper <hannah.c.lepper@gmail.com>
2017-08-02 12:04:35 +0100

    Simple line graphs, ablines

--- graphs.R
+++ graphs.R
@@ -1,13 +1,18 @@
-library(ggplot2)
+#Graphs functions
+#Simple graphs ====
 
-singlelinegraph <- function(data, y) {
-  ggplot(data, aes(x = time, y = y)) +
-    geom_line()
+#Add lines for locations of surveys onto graph
+surveyablines <- function(int){
+  abline(v=survey_times(int)[1])
+  abline(v=survey_times(int)[4])
+  abline(v=survey_times(int)[7])
+  mtext("*", side=1, at = survey_times(int)[c(1,4,7)])
 }
-
-singlelinegraph(sol_base_df, sol_base_df$Inc)
-
-ggplot(sol_base_df) +
-  geom_line(mapping = aes(x = time, y = sol_base_df$I), color = "red") +
-  geom_line(mapping = aes(x = time, y = sol_base_df$N), color = "blue") +
-  legend()
+#Single line plots
+simpleplot <- function(x, y, int, col){
+  plot(x, y, col = col, xlim = c(499, 499+(int*3 + 5)), type = "l")
+}
+#Add another line
+addlines <-function(x, y, col){
+  lines(x, y, col = col, type = "l")
+}
