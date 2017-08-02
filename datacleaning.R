3bddf15865faa15055c3a16130e7f17656d467d7
Hannah Lepper <hannah.c.lepper@gmail.com>
2017-08-02 12:05:22 +0100

    Handle large csv; fix incidences; shorten

--- datacleaning.R
+++ datacleaning.R
@@ -1,22 +1,20 @@
 #Data cleaning
 
-#3000 time points per run
+#3001 time points per run
 
 rebind <- function(chars) {paste(chars, collapse = "")}
 largeexperimentaldataset <- read.csv("C://Users/hanna/Dropbox/Academic/LSHTM/Project/Inputs and outputs/expdata.csv")
 
-#Aim - get series of datasets of experimental inputs and Inc
+#Aim - get series of datasets
 experimentdata[,44] <- rep(c(1:144), each = 30001)
+experimentdata <- experimentdata[,-c(1:14, 17, 20)] #CHECK - get rid of all input parameters
 #split into list of dataframes by experiment
-experimentdatalist <- split(experimentdata, f=experimentdata[,44])
+experimentdatalist <- split(experimentdata, f=experimentdata[,23])
 #name list elements
 namesforlist <- NULL
-for (i in 1:length(experimentdatalist)){
-  set <- experimentdatalist[[i]][1,]
-  namesforlist <- c(namesforlist, rebind(c("r", as.character(set["r"]),
-                                            "k", as.character(set["k"]),
-                                            "cov", as.character(set["cov"]),
-                                            "survey_interval", as.character(set["survey_interval"]))))
+for (i in 1:144){
+  namesforlist <- c(namesforlist, paste(paste(c("r", "k", "cov", "s_i"), 
+                                              as.character(intandbasepars[i,], sep = "_")), 
+                                        collapse = "-"))
 }
-
 names(experimentdatalist) <- namesforlist
