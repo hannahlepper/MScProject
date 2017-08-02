1e5b22b66c842f9d757584eb750771d7edcf8c2f
Hannah Lepper <hannah.c.lepper@gmail.com>
2017-08-02 12:04:52 +0100

    Edits

--- first graph.R
+++ first graph.R
@@ -1,16 +1,20 @@
-function(df){
-  plot(df$time, df$Inc, type = "l")
-  tit
-}
-
+#not working
 efficientsurveylowcomm <- experimentdatalist[[
-  which(names(experimentdatalist)=="r0.1k1cov0.9survey_interval1")[1]]]
+  which(names(experimentdatalist)=="r 0.1-k 1-cov 0.9-s_i 1")[1]]]
 efficientsurveyhighcomm <- experimentdatalist[[
-  which(names(experimentdatalist)=="r0.7k1cov0.9survey_interval1")[1]]]
+  which(names(experimentdatalist)=="r 0.7-k 1-cov 0.9-s_i 1")[1]]]
 
 plot(efficientsurveylowcomm$time, efficientsurveylowcomm$Inc, type = "l", 
-     xlim=c(499,510), ylim = c(110, 114), col = "red")
+     xlim=c(499,510), col = "red")
 lines(efficientsurveyhighcomm$time, efficientsurveyhighcomm$Inc, col = "blue")
+abline(v=survey_times(1)[1])
+abline(v=survey_times(1)[4])
+abline(v=survey_times(1)[7])
+
+plot(efficientsurveyhighcomm$time, efficientsurveyhighcomm$dur_active_inf_TB,
+     type = "l",
+     col = "green",
+     xlim = c(499, 510))
 abline(v=survey_times(1)[1])
 abline(v=survey_times(1)[4])
 abline(v=survey_times(1)[7])
