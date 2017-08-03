90522c410769885005bb38b3bde1bf32658ded54
Hannah Lepper <hannah.c.lepper@gmail.com>
2017-08-03 09:12:17 +0100

    Rerun baseline data

--- baselinerun.R
+++ baselinerun.R
@@ -12,15 +12,13 @@ source("C://Users/hanna/Documents/GitHub/MSc project/fitting.R")
 #Functions====
 #Try to get as close to equilibrium as possible - may or may not use
 convertdftonumeric <- function(df){
-  pars <- c("p", "a", "vf", "vs", "sg", "x", "nc", "theta", 
-            "Mui", "Mun", "CDR", "CDR_survey", "tau", "k",
-            "r", "c", "Ic", "survey_interval", "b", "Mu")
+  pars <- names(df)
   num <- as.numeric(df)
   setNames(num, pars)
 }
 fastrun <- function(pars) {
   y <- c(U=1-0.2,Ls=0.99*0.2,Lf=0,I=0.01*0.2,N=0,C=0)
-  initrun<-runsteady(y=y,times=c(0,Inf), func=PSmodel, parms=pars)
+  initrun<-runsteady(y=y,times=c(0,Inf), func=PSmodel, parms=convertdftonumeric(pars))
   yinit<-initrun$y
   sol_base<-ode(y=yinit,times=seq(0,500, by=0.02),func=PSmodel,parms=convertdftonumeric(pars))
   sol_base_df <- as.data.frame(sol_base)
@@ -43,8 +41,7 @@ fittedparams <- rename(fittedparams, c("beta" = "b"))
 fittedparams <- fittedparams[,-22]
 
 #generate data====
-
-#baselinedata <- ouputplustime(adply, fittedparams, 1, fastrun)
+baselinedata <- outputplustime(adply, fittedparams, 1, fastrun)
 #1.7 minutes
-#write.csv(baselinedata, ""C://Users/hanna/Dropbox/Academic/LSHTM/Project/Inputs and outputs/baselinedata.csv")
+write.csv(baselinedata, ""C://Users/hanna/Dropbox/Academic/LSHTM/Project/Inputs and outputs/baselinedata.csv")
 
