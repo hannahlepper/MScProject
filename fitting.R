d5b8c69556e85c6d0d66f263e51454d836ac52bf
Hannah Lepper <hannah.c.lepper@gmail.com>
2017-08-08 17:56:53 +0100

    Tidy for sourcing

--- fitting.R
+++ fitting.R
@@ -10,28 +10,28 @@ source("C://Users/hanna/Documents/GitHub/MSc project/Parameter_combinations.R")
 source("C://Users/hanna/Documents/GitHub/MSc project/matchclosestMALDIquant.R")
 
 #====Community mixing par scenarios====
-pars <- list(b=10,
-             Mu=0.006,
-             Mui=0.3,
-             Mun=0.21,
-             a=0.115,
-             theta=0.015,
-             p=0.01,
-             c=0.22,
-             x=0.65,
-             vs=0.0005,
-             vf=0.67,
-             nc=0.2,
-             sg=0.45,
-             CDR=0.77,
-             cov = 0, 
-             k = 0,
-             tau=0.91,
-             r=c(0.1, .4, .7),
-             Ic = 0.001,
-             survey_interval=1)
+#pars <- list(b=10,
+             # Mu=0.006,
+             # Mui=0.3,
+             # Mun=0.21,
+             # a=0.115,
+             # theta=0.015,
+             # p=0.01,
+             # c=0.22,
+             # x=0.65,
+             # vs=0.0005,
+             # vf=0.67,
+             # nc=0.2,
+             # sg=0.45,
+             # CDR=0.77,
+             # cov = 0, 
+             # k = 0,
+             # tau=0.91,
+             # r=c(0.1, .4, .7),
+             # Ic = 0.001,
+             # survey_interval=1)
 
-tofit <- parametercombinations(pars)
+#tofit <- parametercombinations(pars)
 #target incidence: 112.34 p 100000 pa
 
 #====equlibrium functions====
@@ -95,70 +95,70 @@ selectpar <- function(pars, fittingparlist, f.output, f.outputname){
 
 #Manual fitting====
 #1. r = 0.1
-pars <- tofit[1,]
- 
-pars["CDR"] <- selectpar(pars, list(CDR = seq(0.62,0.9,length.out = 10)),
-                       0.79, "treatment_cov")
-pars["b"] <- selectpar(pars, list(b = 8:22),
-                       112.34, "Inc")
-pars["Mu"] <- selectpar(pars, list(Mu = seq(0.003,0.008,length.out = 10)),
-                        112.34, "Inc")
-pars["nc"] <- selectpar(pars, list(nc = seq(0.1,0.3,length.out = 10)),
-                        162.36, "Prev")
-pars["Mui"] <- selectpar(pars, list(Mui = seq(0.11,0.2,length.out = 10)),
-                         14.84, "Mort")
-pars["Mun"] <- selectpar(pars, list(Mun = seq(0.12,0.2,length.out = 10)),
-                         14.84, "Mort")
-pars["CDR"] <- selectpar(pars, list(CDR = seq(0.62,0.9,length.out = 10)),
-                         0.79, "treatment_cov")
-pars["b"] <- selectpar(pars, list(b = 8:22),
-                       112.34, "Inc")
-pars["Mu"] <- selectpar(pars, list(Mu = seq(0.003,0.008,length.out = 10)),
-                        112.34, "Inc")
-pars["nc"] <- selectpar(pars, list(nc = seq(0.1,0.3,length.out = 10)),
-                        162.36, "Prev")
-pars["Mui"] <- selectpar(pars, list(Mui = seq(0.11,0.2,length.out = 10)),
-                         14.84, "Mort")
-pars["Mun"] <- selectpar(pars, list(Mun = seq(0.12,0.2,length.out = 10)),
-                         14.84, "Mort")
-pars["CDR"] <- selectpar(pars, list(CDR = seq(0.62,0.9,length.out = 10)),
-                         0.79, "treatment_cov")
-pars["b"] <- selectpar(pars, list(b = 8:22),
-                       112.34, "Inc")
-pars["Mu"] <- selectpar(pars, list(Mu = seq(0.003,0.008,length.out = 10)),
-                        112.34, "Inc")
-pars["nc"] <- selectpar(pars, list(nc = seq(0.1,0.3,length.out = 10)),
-                        162.36, "Prev")
-pars["Mui"] <- selectpar(pars, list(Mui = seq(0.11,0.2,length.out = 10)),
-                         14.84, "Mort")
-pars["Mun"] <- selectpar(pars, list(Mun = seq(0.12,0.2,length.out = 10)),
-                         14.84, "Mort")
-
-r0.1 <- modelrun(pars) #check...
-
+# pars <- tofit[1,]
+#  
+# pars["CDR"] <- selectpar(pars, list(CDR = seq(0.62,0.9,length.out = 10)),
+#                        0.79, "treatment_cov")
+# pars["b"] <- selectpar(pars, list(b = 8:22),
+#                        112.34, "Inc")
+# pars["Mu"] <- selectpar(pars, list(Mu = seq(0.003,0.008,length.out = 10)),
+#                         112.34, "Inc")
+# pars["nc"] <- selectpar(pars, list(nc = seq(0.1,0.3,length.out = 10)),
+#                         162.36, "Prev")
+# pars["Mui"] <- selectpar(pars, list(Mui = seq(0.11,0.2,length.out = 10)),
+#                          14.84, "Mort")
+# pars["Mun"] <- selectpar(pars, list(Mun = seq(0.12,0.2,length.out = 10)),
+#                          14.84, "Mort")
+# pars["CDR"] <- selectpar(pars, list(CDR = seq(0.62,0.9,length.out = 10)),
+#                          0.79, "treatment_cov")
+# pars["b"] <- selectpar(pars, list(b = 8:22),
+#                        112.34, "Inc")
+# pars["Mu"] <- selectpar(pars, list(Mu = seq(0.003,0.008,length.out = 10)),
+#                         112.34, "Inc")
+# pars["nc"] <- selectpar(pars, list(nc = seq(0.1,0.3,length.out = 10)),
+#                         162.36, "Prev")
+# pars["Mui"] <- selectpar(pars, list(Mui = seq(0.11,0.2,length.out = 10)),
+#                          14.84, "Mort")
+# pars["Mun"] <- selectpar(pars, list(Mun = seq(0.12,0.2,length.out = 10)),
+#                          14.84, "Mort")
+# pars["CDR"] <- selectpar(pars, list(CDR = seq(0.62,0.9,length.out = 10)),
+#                          0.79, "treatment_cov")
+# pars["b"] <- selectpar(pars, list(b = 8:22),
+#                        112.34, "Inc")
+# pars["Mu"] <- selectpar(pars, list(Mu = seq(0.003,0.008,length.out = 10)),
+#                         112.34, "Inc")
+# pars["nc"] <- selectpar(pars, list(nc = seq(0.1,0.3,length.out = 10)),
+#                         162.36, "Prev")
+# pars["Mui"] <- selectpar(pars, list(Mui = seq(0.11,0.2,length.out = 10)),
+#                          14.84, "Mort")
+# pars["Mun"] <- selectpar(pars, list(Mun = seq(0.12,0.2,length.out = 10)),
+#                          14.84, "Mort")
 # 
+# r0.1 <- modelrun(pars) #check...
+# 
+# # 
+# # #2. r = 0.4
+# pars <- tofit[2,]
+# 
+# for (i in names(r0.1)) {
+#  pars[which(names(pars)==i)] <- r0.1[which(names(r0.1) == i)]
+# }
+# 
+# pars["b"] <- selectpar(pars, list(b = 6.5:22),
+#                       125.34, "Inc")
+# r0.4  <- modelrun(pars)  
+# #   
+# # 
 # #2. r = 0.4
-pars <- tofit[2,]
-
-for (i in names(r0.1)) {
- pars[which(names(pars)==i)] <- r0.1[which(names(r0.1) == i)]
-}
-
-pars["b"] <- selectpar(pars, list(b = 6.5:22),
-                      125.34, "Inc")
-r0.4  <- modelrun(pars)  
-#   
+# pars <- tofit[3,]
+# for (i in names(r0.1)) {
+#  pars[which(names(pars)==i)] <- r0.1[which(names(r0.1) == i)]
+# }
+# pars["b"] <- selectpar(pars, list(b = 6.5:22),
+#                       125.34, "Inc")
+# r0.7 <- modelrun(pars)
 # 
-#2. r = 0.4
-pars <- tofit[3,]
-for (i in names(r0.1)) {
- pars[which(names(pars)==i)] <- r0.1[which(names(r0.1) == i)]
-}
-pars["b"] <- selectpar(pars, list(b = 6.5:22),
-                      125.34, "Inc")
-r0.7 <- modelrun(pars)
-
-#Results====
-fittedparams <-cbind(rbind(r0.1,r0.4,r0.7), r = c(0.1, 0.4, 0.7))
-write.csv(fittedparams, 
-          "C://Users/hanna/Dropbox/Academic/LSHTM/Project/Inputs and outputs/fittedparams.csv")
+# #Results====
+# fittedparams <-cbind(rbind(r0.1,r0.4,r0.7), r = c(0.1, 0.4, 0.7))
+# write.csv(fittedparams, 
+#          "C://Users/hanna/Dropbox/Academic/LSHTM/Project/Inputs and outputs/fittedparams.csv")
