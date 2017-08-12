e49d5650ca6e8ce0c9d2c150d3633b726100b5d3
Hannah Lepper <hannah.c.lepper@gmail.com>
2017-08-12 12:48:06 +0100

    Tweaks

--- graphs.R
+++ graphs.R
@@ -8,23 +8,18 @@ surveyablines <- function(int){
   abline(v=survey_times(int)[8])
   mtext("*", side=1, at = survey_times(int)[c(1,4,7)])
 }
-#Single line plots
-simpleplot <- function(data, y, int, col, ...){
-  plot(data$time, data$y, col = col, xlim = c(499, 499+(int*3 + 5)), type = "l", ...)
-}
+
 #Add another line
 addlines <-function(x, y, col){
   lines(x, y, col = col, type = "l")
 }
 
 #not working
-multilinegraph <- function(data, var){
-  plot(0, type = "l", xlim = c(499, 510), 
+simplemultilinegraph <- function(data){
+  plot(0, type = "l", 
        ylim = c(
-         min(data[,var])-5, 
-         max(data[,var])+5))
-  for (i in 1:length(unique(data$expnum))){
-    newdata <- subset(data, data$expnum == unique(data$expnum)[i])
-    line(newdata$time, newdata[,var])
-  }
+         min(data$Inc)-5, 
+         max(data$Inc)+5))
+  
+  
 }
